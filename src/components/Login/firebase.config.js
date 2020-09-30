c44295d171ef5272211de28477b605aa8c3f5625
Rezowan-Ahmed <ahmedrezowan07@gmail.com>
2020-09-30 21:05:27 +0800

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA8BB__Nz8RZTFClTkeUP3gHVvpZ9XnZUM",
+    authDomain: "hotel-of-ctgu.firebaseapp.com",
+    databaseURL: "https://hotel-of-ctgu.firebaseio.com",
+    projectId: "hotel-of-ctgu",
+    storageBucket: "hotel-of-ctgu.appspot.com",
+    messagingSenderId: "965767441154",
+    appId: "1:965767441154:web:0514498b43eee66bfeb01b"
+  };
+  export default firebaseConfig;
